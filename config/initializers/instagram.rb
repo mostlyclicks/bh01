34870c65a81666fb0ada91be004b83036b945121
mostlyclicks <carlos@mostlyclicks.com>
2014-04-22 11:05:25 -0500

    Update instagram.rb

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -2,8 +2,8 @@ require "instagram"
 
 Instagram.configure do |config|
   #config.client_id = ENV['INST_ID']
-  config.client_id = '422c0be7e441403daab1b330b0c9fb15'
+  config.client_id = ENV['INST_ID']
   # config.access_token = ENV['INST_ACCESS_TOKEN']
-  config.access_token = '363032975.1fb234f.078dc5a901eb4abb900dae31deebe1ea'
+  config.access_token = ENV['INST_ACCESS_TOKEN']
   
 end
