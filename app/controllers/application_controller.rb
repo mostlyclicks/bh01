5ff056612dad9496d0faa59845d9aeb753dac29c
Carlos Interone <carlos@mostlyclicks.com>
2014-06-07 18:25:23 -0500

    redesign simpler page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :shop_image
+  # before_filter :shop_image
 
   protected
 
-    def shop_image
-      @photos = Instagram.user_recent_media("813985406", {:count => 3}) #363032975 #813985406
-    end
+    # def shop_image
+    #   @photos = Instagram.user_recent_media("813985406", {:count => 3}) #363032975 #813985406
+    # end
 end
